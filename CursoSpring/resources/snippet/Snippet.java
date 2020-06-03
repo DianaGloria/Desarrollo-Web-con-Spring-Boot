0dd6cdadb105383ae31e565af048a6c7dfcc3677
MONIC <MONIC@192.168.1.75>
2020-06-03 05:28:41 -0600

    Commit Creacion del proyecto

---
+++ CursoSpring/resources/snippet/Snippet.java
@@ -0,0 +1,22 @@
+package snippet;
+
+public class Snippet {
+	 spring.thymeleaf.cache = true # Si se debe habilitar el almacenamiento en caché de plantillas. 
+	spring.thymeleaf.check-template = true # Si se debe verificar si la plantilla existe antes de representarla. 
+	spring.thymeleaf.check-template-location = true# Si se debe verificar que existe la ubicación de las plantillas. 
+	spring.thymeleaf.enabled = true # Si se debe habilitar la resolución de vista Thymeleaf para marcos web. 
+	spring.thymeleaf.enable-spring-el-compiler = false # Habilita el compilador SpringEL en las expresiones SpringEL. 
+	spring.thymeleaf.encoding = UTF-8 # Codificación de archivos de plantilla. 
+	spring.thymeleaf.excluded-view-names = # Lista de nombres de vista separados por comas (patrones permitidos) que deben excluirse de la resolución. 
+	spring.thymeleaf.mode = HTML # Modo de plantilla que se aplicará a las plantillas. Vea también la enumeración TemplateMode de Thymeleaf. 
+	spring.thymeleaf.prefix = classpath: / templates /# Prefijo que se antepone para ver nombres al crear una URL. 
+	spring.thymeleaf.reactive.chunked-mode-view-names = # Lista de nombres de vista separados por comas (patrones permitidos) que deberían ser los únicos ejecutados en modo CHUNKED cuando se establece un tamaño de fragmento máximo. 
+	spring.thymeleaf.reactive.full-mode-view-names = # Lista de nombres de vista separados por comas (patrones permitidos) que deben ejecutarse en modo COMPLETO incluso si se establece un tamaño de fragmento máximo. 
+	spring.thymeleaf.reactive.max-chunk-size = 0 # Tamaño máximo de las memorias intermedias de datos utilizadas para escribir en la respuesta, en bytes. 
+	spring.thymeleaf.reactive.media-types = # Tipos de medios compatibles con la tecnología de visualización. 
+	spring.thymeleaf.servlet.content-type= texto / html # Valor de tipo de contenido escrito en respuestas HTTP. 
+	spring.thymeleaf.suffix = .html # Sufijo que se agrega para ver los nombres al crear una URL. 
+	spring.thymeleaf.template-resolver-order = # Orden de la plantilla de resolución en la cadena. 
+	spring.thymeleaf.view-names = # Lista de nombres de vista separados por comas (patrones permitidos) que se pueden resolver. 
+}
+
